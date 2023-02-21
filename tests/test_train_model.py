3492e963273f14e08e6a763b22625629d1f8ae9c
Martijn N <martijn.naayer@chello.nl>
2023-02-21 06:36:20 +0100

    remove space from data

---
+++ tests/test_train_model.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import pandas as pd
+import pytest
+
+from ..src.data_processing.ml import preprocess_data as ppd
+
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_FOLDER = 'data'
+DATA_FILE = 'census.csv'
+
+ 
+@pytest.fixture(scope="module")
+def input_df():
+    df = pd.read_csv(os.path.join(ROOT, DATA_FOLDER, DATA_FILE), sep=',')
+    return df
