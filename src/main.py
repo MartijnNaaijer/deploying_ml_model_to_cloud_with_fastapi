edd8b7796734fd2bbbb61ac30f88393eee814932
Martijn N <martijn.naayer@chello.nl>
2023-02-22 06:55:47 +0100

    pydantic data model

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+class InputData(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education_num: int = Field(alias="education-num")
+    marital_status: str = Field(alias="marital-status")
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int = Field(alias="capital-gain")
+    capital_loss: int = Field(alias="capital-loss")
+    hours_per_week: int = Field(alias="hours_per_week")
+    native_country: str = Field(alias="native-country")
+
+    class Config:
+        allow_population_by_field_name = True
 
 app = FastAPI()
 
 @app.get('/')
 async def say_hello():
     return({'greeting': 'Hi, what about making an inference?'})
+
+@app.post('/inference')
+async def make_inference():
+    pass
